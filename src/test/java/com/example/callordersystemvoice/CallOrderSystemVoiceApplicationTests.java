bf00c3fee6c913697731854aa30feaa4d144cc9f
RickieSayHi <melt_1026@163.com>
2023-04-25 15:15:33 +0800

    初始化项目，2023年4月25日15:15:31

---
+++ src/test/java/com/example/callordersystemvoice/CallOrderSystemVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.callordersystemvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallOrderSystemVoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
